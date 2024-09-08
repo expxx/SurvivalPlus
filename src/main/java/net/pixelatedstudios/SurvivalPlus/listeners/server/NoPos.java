d9bc835ea2380568fb16a719aca6a8e8acbb4c03
Cam <chrlew082@gmail.com>
2024-09-08 13:16:51 -0600

    updating

--- src/main/java/net/pixelatedstudios/SurvivalPlus/listeners/server/NoPos.java
+++ src/main/java/net/pixelatedstudios/SurvivalPlus/listeners/server/NoPos.java
@@ -3,9 +3,9 @@ package net.pixelatedstudios.SurvivalPlus.listeners.server;
 
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.PacketContainer;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.pixelatedstudios.SurvivalPlus.Survival;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -13,9 +13,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerGameModeChangeEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 public class NoPos implements Listener {
     /**
@@ -31,7 +28,7 @@ public class NoPos implements Listener {
             container.getBytes().write(0, (byte)22);
             Survival.getInstance().getProtocolManager().sendServerPacket(player, container);
         } catch (Exception e) {
-            Bukkit.getConsoleSender().sendMessage("[SurvivalPlus] " + ChatColor.RED + e.getMessage());
+            Bukkit.getConsoleSender().sendMessage("[SurvivalPlus] " + NamedTextColor.RED + e.getMessage());
         }
     }
 
@@ -43,18 +40,13 @@ public class NoPos implements Listener {
     @SuppressWarnings("WeakerAccess")
     public static void enableF3(Player player) {
         try {
-            EntityPlayer player1 = (EntityPlayer) getHandle(player);
-            PacketPlayOutEntityStatus packet = new PacketPlayOutEntityStatus(player1, (byte) 23);
-            player1.b.a(packet);
+            PacketContainer container = new PacketContainer(PacketType.Play.Server.ENTITY_STATUS);
+            container.getIntegers().write(0, player.getEntityId());
+            container.getBytes().write(0, (byte)23);
+            Survival.getInstance().getProtocolManager().sendServerPacket(player, container);
         } catch (Exception e) {
-            Bukkit.getConsoleSender().sendMessage("[SurvivalPlus] " + ChatColor.RED + e.getMessage());
+            Bukkit.getConsoleSender().sendMessage("[SurvivalPlus] " + NamedTextColor.RED + e.getMessage());
         }
-    }
-
-    private static Object getHandle(Player player)
-            throws SecurityException, NoSuchMethodException, IllegalArgumentException, IllegalAccessException, InvocationTargetException {
-        Method getHandle = player.getClass().getMethod("getHandle");
-        return getHandle.invoke(player);
     }
 
     @EventHandler
